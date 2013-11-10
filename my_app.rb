f5220ed61bab11437a12d9acda61ac77f4931c81
obliviusm <obliviusm@gmail.com>
2013-11-10 17:48:44 +0200

    before filter

--- my_app.rb
+++ my_app.rb
@@ -1,9 +1,10 @@
 require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
-	
-   # An in-memory Sqlite3 connection:
-DataMapper.setup(:default, 'sqlite::memory:')
+
+enable :sessions	
+
+DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/my_db.db")
 
 class User
 	include DataMapper::Resource
@@ -14,14 +15,29 @@ class User
 end
 
 DataMapper.finalize.auto_upgrade!
-
+=begin
 @user = User.create(
 	login: 		"misha",
 	password: 	"pass"
 )
 @user.save
+=end
+['/users', '/'].each do |path|
+	before path do
+		redirect '/login' unless session[:user]
+	end
+end
 
 get '/users' do
 	@users = User.all
 	erb :'users/index'
+end
+
+get '/'  do
+	"Main page!"
+end
+
+
+get '/login' do
+	"Let's login"
 end
