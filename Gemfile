5e14d5d0b509e4cab5e9d7a3211d6659cb5d1edb
obliviusm <obliviusm@gmail.com>
2013-11-10 21:55:48 +0200

    login

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source :rubygems
 gem "sinatra"
 gem "data_mapper"
 gem "dm-sqlite-adapter"
+gem 'rack-flash3'
--- my_app.rb
+++ my_app.rb
@@ -1,8 +1,10 @@
 require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
+require 'rack-flash'
 
 enable :sessions	
+use Rack::Flash
 
 DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/my_db.db")
 
@@ -12,32 +14,51 @@ class User
 	property :id,		Serial
 	property :login,	String
 	property :password,	String
-end
 
-DataMapper.finalize.auto_upgrade!
-=begin
-@user = User.create(
-	login: 		"misha",
-	password: 	"pass"
-)
-@user.save
-=end
-['/users', '/'].each do |path|
-	before path do
-		redirect '/login' unless session[:user]
+	has n, :visits
+
+	def self.auth(login, pass)
+		User.first(login: login, password: pass)
 	end
 end
 
-get '/users' do
-	@users = User.all
-	erb :'users/index'
+class Visit
+	include DataMapper::Resource
+
+	property :id,			Serial
+	property :created_at,	DateTime
+
+	belongs_to :user
 end
 
-get '/'  do
-	"Main page!"
+DataMapper.finalize.auto_upgrade!
+
+User.first_or_create(
+	login: 		"misha",
+	password: 	"pass"
+	)
+
+['/users', '/'].each do |path|
+	before path do
+		redirect '/login' unless session[:login]
+	end
 end
 
+get '/' do
+	@user = User.first(login: session[:login])
+	erb :'index'
+end
 
 get '/login' do
-	"Let's login"
+	erb :'login'
+end
+
+post '/login' do
+	if session[:login] = User.auth(params["login"], params["pass"]).login
+		flash[:notice] = "Hello"
+		redirect '/'
+	else
+		flash[:notice] = "Wrong"
+		redirect '/login'
+	end
 end
