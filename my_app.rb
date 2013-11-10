d306780f99942bf55b8fa4c7ef0b5a55eb88acac
obliviusm <obliviusm@gmail.com>
2013-11-11 01:04:40 +0200

    signup

--- my_app.rb
+++ my_app.rb
@@ -14,8 +14,14 @@ class User
 	property :id,		Serial
 	property :login,	String
 	property :password,	String
+	attr_accessor :password_confirmation
 
 	has n, :visits
+	validates_uniqueness_of :login
+	validates_confirmation_of :password
+	validates_length_of :login, :min => 3
+	validates_presence_of :login
+	validates_presence_of :password
 
 	def self.auth(login, pass)
 		User.first(login: login, password: pass)
@@ -55,13 +61,38 @@ get '/login' do
 end
 
 post '/login' do
+	p params
 	if session[:login] = User.auth(params["login"], params["pass"]).login
-		flash[:notice] = "Hello"
+		flash[:notice] = "Hello, " + session[:login].to_s
 		#add visit
 		User.first(login: session[:login]).visits.create(created_at: Time.now)
 		redirect '/'
 	else
-		flash[:notice] = "Wrong"
+		flash[:error] = "Wrong login/pass"
 		redirect '/login'
+	end
+end
+
+get '/logout' do
+	session[:login] = nil
+	flash[:notice] = "Log out successful"
+	redirect '/login'
+end
+
+get '/signup' do
+	erb :'signup'
+end
+
+post '/signup' do
+	user = User.new(login: params[:login], password: params[:pass], 
+		password_confirmation: params[:pass_conf])
+	if user.save
+		session[:login] = User.auth(params["login"], params["pass"]).login
+		flash[:notice] = "Hello, " + session[:login].to_s
+		User.first(login: session[:login]).visits.create(created_at: Time.now)
+		redirect '/'
+	else
+		flash[:error] = user.errors
+		redirect '/signup'
 	end
 end
